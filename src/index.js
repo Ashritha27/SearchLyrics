91b99be9a83cb50990b7a962cfa2ca71fce0ba9c
Ashritha27 <35839006+Ashritha27@users.noreply.github.com>
2022-01-25 10:00:00 +0530

    Commit

--- src/index.js
+++ src/index.js
@@ -6,12 +6,31 @@ const more = document.getElementById("more");
 const apiURL = "https://api.lyrics.ovh";
 
 //Search
-function searchSongs(searchTerm) {
-  fetch(`${apiURL}/suggest/${searchTerm}`)
-    .then((res) => res.json())
-    .then((data) => console.log(data));
+async function searchSongs(searchTerm) {
+  const res = await fetch(`${apiURL}/suggest/${searchTerm}`);
+  const data = await res.json();
+
+  showData(data);
 }
 
+function showData(data) {
+  let output = "";
+  data.data.forEach((song) => {
+    output += `
+    <li>
+      <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+      <button class="btn" data-artist="${song.artist.name}" 
+      data-songtitle="${song.title}">Get Lyrics</button>
+    </li>
+    `;
+  });
+
+  result.innerHTML = `
+  <ul class="songs">
+   ${output}
+  </ul>
+  `;
+}
 //Event lisytender
 form.addEventListener("submit", (e) => {
   e.preventDefault();
